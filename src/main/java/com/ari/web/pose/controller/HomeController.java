4209122c4cb06f968216e559e10bb23832f1395d
Arindam Mac <arindammitra06@gmail.com>
2022-10-24 23:40:12 -0400

    Initial
    initial commit

---
+++ src/main/java/com/ari/web/pose/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.ari.web.pose.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String home() {
+
+        return "welcome";
+    }
+    
+    
+    @RequestMapping("/pose")
+    public String test() {
+        return "pose";
+    }
+}
